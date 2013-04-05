b942f186fab715509180892880c67e6760062ecc
Jon Phenow <j.phenow@gmail.com>
2013-04-05 14:23:18 -0500

    implementation

--- lib/okcomputer-newrelic-ignore.rb
+++ lib/okcomputer-newrelic-ignore.rb
@@ -1,9 +1,9 @@
 require "okcomputer-newrelic-ignore/version"
 
-module Okcomputer
-  module Newrelic
-    module Ignore
-      # Your code goes here...
-    end
-  end
+require 'okcomputer'
+require File.join OKComputer::Engine.paths["app/controllers"].expanded.first, "ok_computer_controller.rb"
+require 'new_relic/agent/instrumentation/controller_instrumentation'
+class OkComputerController
+  include NewRelic::Agent::Instrumentation::ControllerInstrumentation
+  newrelic_ignore
 end
