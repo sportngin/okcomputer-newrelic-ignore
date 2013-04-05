b37286eff17b2dab7f3c60e735fa146f79eb4531
Jon Phenow <j.phenow@gmail.com>
2013-04-05 14:17:07 -0500

    initial create

---
+++ lib/okcomputer-newrelic-ignore.rb
@@ -0,0 +1,9 @@
+require "okcomputer-newrelic-ignore/version"
+
+module Okcomputer
+  module Newrelic
+    module Ignore
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/okcomputer-newrelic-ignore/version.rb
@@ -0,0 +1,7 @@
+module Okcomputer
+  module Newrelic
+    module Ignore
+      VERSION = "0.0.1"
+    end
+  end
+end
